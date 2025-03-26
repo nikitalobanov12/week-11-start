7b5fa2c974e5e0f7462fa97a3d79326c48e6cef2
nikitalobanov12 <nikitaalobanovv@gmail.com>
2025-03-26 13:53:16 -0700

    please

--- databaseConnection.js
+++ databaseConnection.js
@@ -1,14 +1,19 @@
-const MongoClient = require("mongodb").MongoClient;
+const { MongoClient } = require("mongodb");
+
 const is_hosted = process.env.IS_HOSTED || false;
 const hostedURI =
-"mongodb+srv://theMongoAdmin:accidentalLoginSteps@cluster0.4ulcc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-const localURI = "mongodb://127.0.0.1/?authSource=admin&retryWrites=true&w=majority"
-if (is_hosted) {
-var database = new MongoClient(hostedURI,
- {useNewUrlParser: true, useUnifiedTopology: true});
-}
-else {
-var database = new MongoClient(localURI,
- {useNewUrlParser: true, useUnifiedTopology: true});
-}
+    "mongodb+srv://theMongoAdmin:accidentalLoginSteps@cluster0.o7qbwyx.mongodb.net/";
+const localURI = "mongodb://127.0.0.1/";
+
+const database = new MongoClient(is_hosted ? hostedURI : localURI);
+
+(async () => {
+    try {
+        await database.connect();
+        console.log("Connected to MongoDB");
+    } catch (err) {
+        console.error("Error connecting to MongoDB:", err);
+    }
+})();
+
 module.exports = database;
